3c4c50d392c473324adfbca811be1614b85920ae
Anthony Benjamin <landdeld@live.com>
2021-03-24 21:36:45 -0500

    Add create customer functionality

--- src/main/java/com/example/demo/custController/CustCon.java
+++ src/main/java/com/example/demo/custController/CustCon.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
 
 @RestController
@@ -14,7 +17,7 @@ public class CustCon {
 	CustomerRepository myclass;
 
 	//gets
-	@GetMapping("/customers")
+	@GetMapping("/")
 	public String getAll() {
 		return myclass.findAll().toString();
 	}
@@ -34,6 +37,10 @@ public class CustCon {
 	  }
 	
 	//create
+	@PostMapping("/customers")
+	public String createcustmoer(@RequestBody Customer newCust) {
+		return myclass.save(newCust).toString();		
+	}
 	
 	//put
 }
