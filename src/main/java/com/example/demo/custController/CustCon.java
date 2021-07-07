33099b914bc5fd5d7836850a6d27f309c2fb7a2e
Anthony Benjamin <landdeld@live.com>
2021-07-07 18:18:29 -0500

    first commit

--- src/main/java/com/example/demo/custController/CustCon.java
+++ src/main/java/com/example/demo/custController/CustCon.java
@@ -1,17 +1,24 @@
 package com.example.demo.custController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Customer;
 import com.example.demo.repository.CustomerRepository;
 
+@CrossOrigin
 @RestController
 public class CustCon {
 	@Autowired
@@ -19,8 +26,10 @@ public class CustCon {
 
 	//gets
 	@GetMapping("/customers")
-	public String getAll() {
-		return myclass.findAll().toString();
+	public List<Customer> getAll() {
+		List<Customer> customers = new ArrayList<Customer>();
+		myclass.findAll().forEach(customers::add);
+		return customers;
 	}
 	
 	@GetMapping("/customers/{id}")
